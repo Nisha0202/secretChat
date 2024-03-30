1ad53676d525fd413be275b8688193eeffe5e440
Nisha0202 <nishajabatunnessa@gmail.com>
2024-03-31 02:26:43 +0600

    3D card scrolling

--- src/pages/SecretSpace.jsx
+++ src/pages/SecretSpace.jsx
@@ -1,28 +1,35 @@
-
 import React, { useRef, useState } from 'react';
-import { Virtual, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import './Space.css'
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+// Import your components
 import Logo from '../components/Logo';
 import Search from '../components/Search';
 import Card from '../components/Card';
+import './Space.css';
 
 export default function SecretSpace() {
+
+  // Swiper instance reference
   const [swiperRef, setSwiperRef] = useState(null);
-  // Create array with 100 Card components
-  const [slides, setSlides] = useState(
-    Array.from({ length: 50 }).map((_, index) => <Card key={index} />)
+
+  // Slides array
+  const slides = Array.from({ length: 100 }).map(
+    (el, index) => <Card key={index} />
   );
 
+  // Slide to a specific slide
   const slideTo = (index) => {
     swiperRef.slideTo(index - 1, 0);
   };
+
+
   return (
     <>
       <div className='fullscreen-container Lato py-16'>
@@ -30,52 +37,47 @@ export default function SecretSpace() {
           <Logo />
           <Search />
           <Swiper
-            slidesPerView={'auto'}
-            spaceBetween={20}
-            modules={[Virtual, Navigation, Pagination]}
             onSwiper={setSwiperRef}
+            effect={'coverflow'}
+            grabCursor={true}
             centeredSlides={true}
-            pagination={{
-              type: 'fraction',
+            loop={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 1.5,
             }}
             navigation={true}
-            virtual
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className="swiper_container"
             breakpoints={{
               // when window width is >= 640px
               640: {
-                slidesPerView: 2,
-                spaceBetween: 20
-
+                slidesPerView: 1,
+                spaceBetween: 10,
               },
               // when window width is >= 768px
               768: {
-                slidesPerView: 3,
-                spaceBetween: 20
-              },
-              // when window width is >= 1024px
-              1024: {
                 slidesPerView: 4,
-                spaceBetween: 30
+                spaceBetween: 30,
               },
-            }} >
-            {slides.map((slideContent, index) => (
-              <SwiperSlide key={slideContent} virtualIndex={index}>
-                {slideContent}
+            }}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide key={index}>
+                {slide}
               </SwiperSlide>
             ))}
           </Swiper>
 
           {/*scroll to start or end */}
           <p className="append-buttons md:flex md:justify-between w-full hidden">
-            <button onClick={() => slideTo(1)} className="prepend-slide btn btn-sm w-16 bg-myblack border-2 border-mygray text-mygray">
-              start
-            </button>
-            <button onClick={() => slideTo(500)} className="slide-500 btn btn-sm w-16 bg-myblack border-mygray text-mygray">
-              end
-            </button>
+            <button onClick={() => slideTo(1)} className="prepend-slide btn btn-sm w-16 bg-myblack border-2 border-mygray text-mygray"> start </button>
+            <button onClick={() => slideTo(500)} className="slide-500 btn btn-sm w-16 bg-myblack border-mygray text-mygray">  end          </button>
 
           </p>
-
         </div>
       </div>
     </>

5658808b23c07621adec360c815b408a323f600a
Nisha0202 <nishajabatunnessa@gmail.com>
2024-04-05 05:04:29 +0600

    chatbubble name

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -4,7 +4,8 @@ export default function ChatSection() {
     const [messages, setMessages] = useState([]);
     const [input, setInput] = useState('');
 
-    const handleSend = () => {
+    const handleSend = (e) => {
+        e.preventDefault();
         setMessages([...messages, input]);
         setInput('');
     };
@@ -12,28 +13,41 @@ export default function ChatSection() {
         <div className="flex flex-col h-full lato">
             <nav className="bg-blue-500 p-4 text-white lato border-b-[1px] border-myblack flex justify-between">
                 <div>Chat Room Name</div>
-                <div><RxDotsHorizontal className='text-myBlue' size={24}/></div>  
+                <div><RxDotsHorizontal className='text-myBlue' size={24} /></div>
             </nav>
-            <div className="flex-grow overflow-auto p-4 space-y-2 chat chat-end">
-                {messages.map((message, index) => (
+            <div className="flex-grow overflow-auto p-4 space-y-2">
+                {/* chat bubble */}            
+                {messages.map((message, index) => (<div className='chat chat-end'>
+
+                      <div className="chat-header">Anakin
+                    <time className="text-xs opacity-50">12:46</time>
+                </div>
                     <div key={index} className=" chat-bubble p-2 bg-myblack rounded-md text-right">
                         {message}
                     </div>
-                ))}
-            </div>
-            <div className="p-4 bg-gray-200 flex items-center space-x-2 border-t-[1px] border-myblack">
-                <input 
-                    type="text"
-                    value={input} 
-                    onChange={e => setInput(e.target.value)} 
-                    className="textarea text-mygray flex-grow rounded-md border p-2"   />
-                <button 
-                    onClick={handleSend} 
-                    className="btn btn-sm bg-mypink border-0 text-myblack rounded-md px-4 py-2 lato" >
-                    Send
-                </button>
-            </div>
+                    <div className="chat-footer opacity-50">
+                        Seen at 12:46
+                    </div></div>
+                ))}       
+            </div >
+
+        <div className="p-4 bg-gray-200 space-x-2 border-t-[1px] border-myblack">
+            <form action="" className='w-full flex gap-4 items-center'>
+                 <input
+                type="text"
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                className=" text-mygray flex-grow rounded-md border p-2" />
+            <button
+            type='submit'
+                onClick={handleSend}
+                className="btn btn-sm flex-shrink-0 bg-mypink border-0 text-myblack rounded-md px-4 py-2 lato" >
+                Send
+            </button>
+            </form>
+           
         </div>
+        </div >
     );
 }
 
